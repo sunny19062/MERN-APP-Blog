9e698a6add84fe819f49b91529a4ba3835826123
Sunny Kumar <108226025+sunny19062@users.noreply.github.com>
2023-08-22 22:07:49 +0530

    Update image-controller.js

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -1,7 +1,7 @@
 import grid from 'gridfs-stream';
 import mongoose from 'mongoose';
 
-const url = 'http://localhost:8000';
+const url = 'https://blog-server-yqf8.onrender.com/';
 
 
 let gfs, gridfsBucket;
